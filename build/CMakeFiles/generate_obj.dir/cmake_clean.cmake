18c7bd3dd2b8d7bebb777325b104742ce5630b0d
fariswirakusuma <fariswkt@gmail.com>
2026-03-24 16:09:13 +0700

    chore:modify .gitignore

--- build/CMakeFiles/generate_obj.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/faris-wirakusuma/Ngampus/STIMA/Tucil2_13524041_13524130/bin/output.obj"
-  "CMakeFiles/generate_obj"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/generate_obj.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/view.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/view"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/view.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
